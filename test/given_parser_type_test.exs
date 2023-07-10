95ab1f75fd13e2267fb3905b2a44aec5c6b4aabd
J Every <devstopfix@gmail.com>
2023-07-10 18:17:53 +0100

    feat: parser type tests

---
+++ test/given_parser_type_test.exs
@@ -0,0 +1,14 @@
+defmodule Given.ParserTypesTest do
+  use ExUnit.Case
+  use Given.Case
+
+  describe "Parse type" do
+    scenario "date", ~s[Given date 2014-09-18]
+    scenario "integer", ~s[Given integer 0]
+    scenario "string", ~s[Given string "a & b"]
+  end
+
+  def given_({:date, ~D[2014-09-18]}, _), do: true
+  def given_({:integer, 0}, _), do: true
+  def given_({:string, "a & b"}, _), do: true
+end
